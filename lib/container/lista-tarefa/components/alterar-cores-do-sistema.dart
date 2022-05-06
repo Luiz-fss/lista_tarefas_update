9e3a46b21f884d19ad2a8d46757434fc32c62682
Luiz-fss <luiz.fss@hotmail.com>
2022-05-06 01:13:29 -0300

    Inclusão do Shared Preferences

--- lib/container/lista-tarefa/components/alterar-cores-do-sistema.dart
+++ lib/container/lista-tarefa/components/alterar-cores-do-sistema.dart
@@ -4,6 +4,9 @@ import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:lista_tarefas_atualizado/container/lista-tarefa/bloc/lista-tarefa-cubit-model.dart';
 import 'package:lista_tarefas_atualizado/container/lista-tarefa/bloc/lista-tarefa-cubit.dart';
 import 'package:lista_tarefas_atualizado/utils/card-alteracao-cores.dart';
+import 'package:lista_tarefas_atualizado/utils/tipo-edicao-cor.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class AlterarCoresDoSistema extends StatefulWidget {
   const AlterarCoresDoSistema({Key key}) : super(key: key);
@@ -15,6 +18,7 @@ class AlterarCoresDoSistema extends StatefulWidget {
 
 class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
   ListaTarefaCubit _bloc;
+  SharedPreferences _sharedPreferences;
   @override
   Widget build(BuildContext context) {
     _bloc = ModalRoute.of(context).settings.arguments;
@@ -49,8 +53,13 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor da App Bar",
-              funcaoDoCard: (){
-                editarCorDaAppBar(context);
+              funcaoDoCard: ()async{
+                await editarCorDaAppBar(context);
+                _sharedPreferences =await SharedPreferences.getInstance();
+                _sharedPreferences.setInt(
+                  TipoEdicaoCorSelecao[TipoEdicaoCor.COR_APP_BAR],
+                    _bloc.state.corAppBar.value
+                );
               },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
@@ -59,8 +68,12 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor do título da App Bar",
-              funcaoDoCard: (){
-                editarCorTituloDaAppBar(context);
+              funcaoDoCard: ()async{
+                await editarCorTituloDaAppBar(context);
+                _sharedPreferences =await SharedPreferences.getInstance();
+                _sharedPreferences.setInt(
+                    TipoEdicaoCorSelecao[TipoEdicaoCor.COR_TITULO_APP_BAR],
+                    _bloc.state.corTituloAppBar.value);
               },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
@@ -69,8 +82,11 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor do Botão de Adiconar Tarefa",
-              funcaoDoCard: (){
-                editarCorBotaoAdicionarTarefa(context);
+              funcaoDoCard: ()async{
+                await editarCorBotaoAdicionarTarefa(context);
+                _sharedPreferences.setInt(
+                    TipoEdicaoCorSelecao[TipoEdicaoCor.COR_BOTAO_ADICIONAR_TAREFA],
+                    _bloc.state.corBotaoAdicionarTarefa.value);
               },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
@@ -79,8 +95,11 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor do ícone do Botão de Adiconar Tarefa",
-              funcaoDoCard: (){
-                editarCorIconeAdicionarTarefa(context);
+              funcaoDoCard: ()async{
+                await editarCorIconeAdicionarTarefa(context);
+                _sharedPreferences.setInt(
+                    TipoEdicaoCorSelecao[TipoEdicaoCor.COR_ICONE_ADICIONAR_TAREFA],
+                    _bloc.state.corIconeBotaoAdicionarTarefa.value);
               },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
@@ -89,8 +108,11 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor do ícone de tarefa não concluida",
-              funcaoDoCard: (){
-                editarCorIconeTarefaNaoConcluida(context);
+              funcaoDoCard: ()async{
+                await editarCorIconeTarefaNaoConcluida(context);
+                _sharedPreferences.setInt(
+                    TipoEdicaoCorSelecao[TipoEdicaoCor.COR_ICONE_TAREFA_NAO_CONCLUIDA],
+                    _bloc.state.corIconeTarefaNaoConcluida.value);
               },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
@@ -99,8 +121,11 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor do fundo de tarefa não concluida",
-              funcaoDoCard: (){
-                editarCorFundoTarefaNaoConcluido(context);
+              funcaoDoCard: ()async{
+               await editarCorFundoTarefaNaoConcluido(context);
+               _sharedPreferences.setInt(
+                   TipoEdicaoCorSelecao[TipoEdicaoCor.COR_FUNDO_TAREFA_NAO_CONCLUIDA],
+                   _bloc.state.corDoFundoTarefaNaoConcluida.value);
               },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
@@ -109,8 +134,11 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor do ícone de tarefa concluida",
-              funcaoDoCard: (){
-                editarCorIconeTarefaConcluido(context);
+              funcaoDoCard: ()async{
+                await editarCorIconeTarefaConcluido(context);
+                _sharedPreferences.setInt(
+                    TipoEdicaoCorSelecao[TipoEdicaoCor.COR_ICONE_TAREFA_CONCLUIDA],
+                    _bloc.state.corIconeTarefaConcluida.value);
               },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
@@ -119,7 +147,12 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
             margin: const EdgeInsets.only(top: 8),
             child: CardAlterarCores(
               tituloCard: "Cor do fundo de tarefa concluida",
-              funcaoDoCard: (){},
+              funcaoDoCard: ()async{
+                await editarCorFundoTarefaConcluido(context);
+                _sharedPreferences.setInt(
+                    TipoEdicaoCorSelecao[TipoEdicaoCor.COR_FUNDO_TAREFA_CONCLUIDA],
+                    _bloc.state.corDoFundoTarefaConcluida.value);
+              },
               iconeDaOpcao: const Icon(Icons.add_circle,color: Colors.green,),
             ),
           ),
@@ -165,7 +198,7 @@ class _AlterarCoresDoSistemaState extends State<AlterarCoresDoSistema> {
         content:  SingleChildScrollView(
           child:  ColorPicker(
             pickerColor: Colors.blue,
-            onColorChanged: (Color changeColor){
+            onColorChanged: (Color changeColor)async{
               _bloc.state.corTituloAppBar = changeColor;
             },
             showLabel: true,
